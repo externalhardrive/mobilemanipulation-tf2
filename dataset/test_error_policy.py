ccb46d99476bddf0959290a3628ff38e0c993bd9
Charles Sun <charlesjsun@gmail.com>
2020-04-30 01:49:48 -0700

    testing code

--- dataset/test_error_policy.py
+++ dataset/test_error_policy.py
@@ -3,6 +3,7 @@ import json
 import os
 from pathlib import Path
 import pickle
+import glob
 
 import pandas as pd
 import numpy as np
@@ -11,6 +12,7 @@ from collections import OrderedDict
 
 from softlearning.environments.utils import get_environment_from_params
 from softlearning import policies
+from softlearning import replay_pools
 from softlearning.samplers import rollouts
 from softlearning.utils.tensorflow import set_gpu_memory_growth
 from softlearning.utils.video import save_video
@@ -46,15 +48,39 @@ def load_policy(checkpoint_dir, variant, environment):
 
     return policy
 
-root_path = "/home/externalhardrive/ray_results/gym/Locobot/MixedNavigation-v0/2020-04-25T20-21-03-locobot-mixed-navigation-test/id=00000-seed=9994_0_hidden_layer_sizes=(256, 256),preprocessors=({'pixels': {'class_name': 'convnet_preprocessor', 'config': {'co_2020-04-25_20-21-040wu9t7n2/"
+def load_replay_pool(ray_path, variant, env):
+    variant['replay_pool_params']['config'].update({'environment': env})
+    replay_pool = replay_pools.get(variant['replay_pool_params'])
 
-variant_path = root_path + "params.pkl"
+    experiment_root = os.path.dirname(ray_path)
+
+    experience_paths = [os.path.join(checkpoint_dir, 'replay_pool.pkl') for checkpoint_dir in sorted(glob.iglob(os.path.join(experiment_root, 'checkpoint_*')))]
+    for experience_path in experience_paths:
+        replay_pool.load_experience(experience_path)
+
+    return replay_pool
+
+# root_path = "/home/externalhardrive/ray_results/gym/Locobot/MixedNavigation-v0/2020-04-25T20-21-03-locobot-mixed-navigation-test/id=00000-seed=9994_0_hidden_layer_sizes=(256, 256),preprocessors=({'pixels': {'class_name': 'convnet_preprocessor', 'config': {'co_2020-04-25_20-21-040wu9t7n2/"
+# variant_path = root_path + "params.pkl"
+# policy_path = root_path + "checkpoint_54/policy"
+
+ray_path = "/home/externalhardrive/ray_results/gym/Locobot/MixedNavigation-v0/2020-04-25T15-24-40-locobot-mixed-navigation-test/id=00000-seed=9994_0_hidden_layer_sizes=(256, 256),preprocessors=({'pixels': {'class_name': 'convnet_preprocessor', 'config': {'co_2020-04-25_15-24-41r2mxmsbx/"
+root_path = "nohup_output/error_2/error_policy_8406/"
+variant_path = ray_path + "params.pkl"
+final_policy_path = root_path + "policy"
+checkpoint_policy_path = ray_path + "checkpoint_7/policy"
+final_path_path = root_path + "curr_path.npy"
+
 with open(variant_path, 'rb') as f:
     variant = pickle.load(f)
 
 env = load_environment(variant, {})
-policy = load_policy(root_path + "checkpoint_54/policy", variant, env)
+final_policy = load_policy(final_policy_path, variant, env)
+checkpoint_policy = load_policy(checkpoint_policy_path, variant, env)
 
 print(variant)
 
 # observation = np.load('nohup_output/error_policy/error_policy/observation.npy', allow_pickle=True)[()]
+replay_pool = load_replay_pool(ray_path, variant, env)
+final_path = np.load(final_path_path, allow_pickle=True)
+
