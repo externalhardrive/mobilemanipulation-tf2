f28464fd3860a992ee6ac9e967cfe5d0ab40f59a
Charles Sun <charlesjsun@gmail.com>
2020-06-26 17:22:53 -0700

    rnd exploration envs

--- softlearning/environments/gym/tests/__init__.py
+++ softlearning/environments/gym/tests/__init__.py
@@ -1,4 +1,3 @@
 from .mixed_action_test_envs import LineReach
 from .sac_grasping_test_envs import LineGrasping, LineGraspingDiscrete
-
-__all__ = ['LineReach', 'LineGrasping', 'LineGraspingDiscrete']
+from .perturbation_test_envs import PointGridExploration
